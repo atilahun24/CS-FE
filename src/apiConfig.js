d82803ea1221f3bd47853f395564e3be6ca1fbb7
atilahun24 <andretilahun@gmail.com>
2019-07-25 23:20:44 -0400

    Remove forward slash for heroku url
    removing a forward slash in the development url that may be causing an
    error

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://whispering-fjord-26435.herokuapp.com/',
+  production: 'https://whispering-fjord-26435.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
